5ed6d6209b8341e2817d77123abec2ca77c4febe
Taskmanager <alexander.hruza@gmx.at>
2026-06-08 15:03:15 +0200

    Update config_flow.py

--- custom_components/shared_family_calendar/config_flow.py
+++ custom_components/shared_family_calendar/config_flow.py
@@ -1,4 +1,4 @@
-"""Config flow for Shared Calendar integration."""
+"""Config flow for Shared Family Calendar."""
 
 import voluptuous as vol
 
@@ -9,21 +9,35 @@ from homeassistant.data_entry_flow import FlowResult
 from .const import DEFAULT_NAME, DOMAIN
 
 
-class SharedCalendarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Shared Calendar."""
+class SharedFamilyCalendarConfigFlow(
+    config_entries.ConfigFlow,
+    domain=DOMAIN,
+):
+    """Handle a config flow for Shared Family Calendar."""
 
     VERSION = 1
 
     async def async_step_user(self, user_input=None) -> FlowResult:
-        """Handle the initial step."""
+        """Handle initial setup."""
+
         if user_input is not None:
-            title = user_input.get(CONF_NAME) or DEFAULT_NAME
-            return self.async_create_entry(title=title, data={CONF_NAME: title})
+            title = user_input.get(CONF_NAME, DEFAULT_NAME)
 
-        data_schema = vol.Schema(
-            {
-                vol.Optional(CONF_NAME, default=DEFAULT_NAME): str,
-            }
+            return self.async_create_entry(
+                title=title,
+                data={
+                    CONF_NAME: title,
+                },
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(
+                        CONF_NAME,
+                        default=DEFAULT_NAME,
+                    ): str,
+                }
+            ),
         )
-
-        return self.async_show_form(step_id="user", data_schema=data_schema)
